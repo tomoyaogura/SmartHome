b1ab8cf09f2b23de97e838235856098290e3241a
Tommy Ogura <tommy8sc@gmail.com>
2018-03-18 11:54:47 -0700

    Bug fix

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET_KEY = os.getenv('SECRET')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(FILE_PATH, DB_NAME) 
 
