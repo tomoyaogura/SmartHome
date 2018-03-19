71b7352cd0f28dc4e8706f92aae482c04d1a230f
Tommy Ogura <tommy8sc@gmail.com>
2018-03-18 23:56:01 -0700

    Add light

--- control.py
+++ control.py
@@ -22,6 +22,7 @@ if is_raspi():
     import RPi.GPIO as GPIO
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
+    GPIO.setup(20, GPIO.OUT)
     GPIO.setup(21, GPIO.OUT)
 
 def turn_on(outlet_id, device_type):
